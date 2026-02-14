fc9d57a10fcabe92c64622900ba28cc71e9e3c8c
Angela <angelastrechen1@mail.com>
2026-02-14 22:35:22 +0100

    Created PostInfo

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -1,25 +1,27 @@
-import './PostInfo.scss';
-
 import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
-import './PostInfo.scss';
 
-export const PostInfo = ({ post }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
+export const PostInfo = ({ post }) => {
+  // Дефолт на порожній масив для безпечного доступу
+  const comments = post.comments || [];
 
-      {post.user && <UserInfo user={post.user} />}
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        {post.user && <UserInfo user={post.user} />}
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      {comments.length > 0 ? (
+        <CommentList comments={comments} />
+      ) : (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      )}
     </div>
-
-    <p className="PostInfo__body">{post.body}</p>
-
-    <hr />
-
-    {post.comments.length > 0 ? (
-      <CommentList comments={post.comments} />
-    ) : (
-      <b data-cy="NoCommentsMessage">No comments yet</b>
-    )}
-  </div>
-);
+  );
+};
